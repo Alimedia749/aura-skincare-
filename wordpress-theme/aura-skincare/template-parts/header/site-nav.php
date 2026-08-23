7c19261d6886395048d6f61ca858a4dbeb45172d
Rayyan Cosmetic <rayyancosmetic@local.dev>
2026-08-23 22:52:48 +0500

    Update header Shop dropdown to match footer Shop menu items (All Products, Bestsellers, New Arrivals, Sets & Kits, Gift Cards)

--- wordpress-theme/aura-skincare/template-parts/header/site-nav.php
+++ wordpress-theme/aura-skincare/template-parts/header/site-nav.php
@@ -67,13 +67,10 @@ $cart_count = ( function_exists( 'WC' ) && WC()->cart ) ? WC()->cart->get_cart_c
 							</a>
 							<ul class="sub-menu">
 								<li><a href="<?php echo esc_url( home_url( '/shop/' ) ); ?>"><?php esc_html_e( 'All Products', 'aura-skincare' ); ?></a></li>
-								<li><a href="<?php echo esc_url( home_url( '/shop/' ) ); ?>"><?php esc_html_e( 'Cleansers', 'aura-skincare' ); ?></a></li>
-								<li><a href="<?php echo esc_url( home_url( '/shop/' ) ); ?>"><?php esc_html_e( 'Serums & Oils', 'aura-skincare' ); ?></a></li>
-								<li><a href="<?php echo esc_url( home_url( '/shop/' ) ); ?>"><?php esc_html_e( 'Moisturizers', 'aura-skincare' ); ?></a></li>
-								<li><a href="<?php echo esc_url( home_url( '/shop/' ) ); ?>"><?php esc_html_e( 'Eye Care', 'aura-skincare' ); ?></a></li>
-								<li><a href="<?php echo esc_url( home_url( '/shop/' ) ); ?>"><?php esc_html_e( 'Toners & Mists', 'aura-skincare' ); ?></a></li>
-								<li><a href="<?php echo esc_url( home_url( '/shop/' ) ); ?>"><?php esc_html_e( 'Sun Protection', 'aura-skincare' ); ?></a></li>
-								<li><a href="<?php echo esc_url( home_url( '/shop/' ) ); ?>"><?php esc_html_e( 'Botanical Oils', 'aura-skincare' ); ?></a></li>
+								<li><a href="<?php echo esc_url( home_url( '/#bestsellers' ) ); ?>"><?php esc_html_e( 'Bestsellers', 'aura-skincare' ); ?></a></li>
+								<li><a href="<?php echo esc_url( home_url( '/shop/' ) ); ?>"><?php esc_html_e( 'New Arrivals', 'aura-skincare' ); ?></a></li>
+								<li><a href="<?php echo esc_url( home_url( '/shop/' ) ); ?>"><?php esc_html_e( 'Sets & Kits', 'aura-skincare' ); ?></a></li>
+								<li><a href="<?php echo esc_url( home_url( '/gift-cards/' ) ); ?>"><?php esc_html_e( 'Gift Cards', 'aura-skincare' ); ?></a></li>
 							</ul>
 						</li>
 						<li><a href="<?php echo esc_url( home_url( '/about-us/' ) ); ?>"><?php esc_html_e( 'About Us', 'aura-skincare' ); ?></a></li>
